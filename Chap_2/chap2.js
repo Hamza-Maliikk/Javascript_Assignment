5c6b654b0d7db88384266252807af4327aac0acb
username like :"Hamza developer26 <hamzascorpio03@gmail.com>
2025-05-25 14:52:33 +0500

    initial commit

---
+++ Chap_2/chap2.js
@@ -0,0 +1,38 @@
+// ques1
+var username;
+
+// ques2
+var myName = "Muhammad Hamza Malik"
+
+// ques3
+var message = "Hello World"
+alert(message)
+
+// ques4
+var stdname = "Muhammad Hamza Malik"
+var age = 30
+var degree = "Certified Full Stack Developer"
+
+alert(stdname)
+alert(age)
+alert(degree)
+
+// ques5
+var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+alert(pizza)
+
+// ques6
+var email = "hamzamalik@gmail.com"
+alert("My email address is " + email)
+
+// ques7
+var book = "A smarter way to learn javascript"
+alert("I am trying to learn this book " + book )
+
+// ques8
+var show = "Yah! i can write HTML content through Javascript"
+document.write(show)
+
+// ques9
+var emoji = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+alert(emoji)
