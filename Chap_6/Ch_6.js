38f2c7dcdd03cef47c6ae134d917a4874f51e405
username like :"Hamza developer26 <hamzascorpio03@gmail.com>
2025-05-25 14:48:41 +0500

    update commit

---
+++ Chap_6/Ch_6.js
@@ -0,0 +1,21 @@
+// ques 1
+var a = 10
+document.write("The value of a is "+ a +"<br>")
+document.write("The value of ++a is "+ ++a +"<br>")
+document.write("Now the value of a is"+ a +"<br>")
+document.write("The value of a++ is "+ a++ +"<br>")
+document.write("Now the value of a is "+ a +"<br>")
+document.write("The value of --a is "+ --a +"<br>")
+document.write("Now the value of a is "+ a +"<br>")
+document.write("Now the value of a is "+ a-- +"<br>")
+document.write("Now the value of a is "+ a +"<br>")
+
+// ques 2
+var a = 2
+var b = 1
+
+var result = --a - --b + ++b + b--;
+
+document.write("a is "+ a +"<br>")
+document.write("b is "+ b +"<br>")
+document.write("result is "+ result)
